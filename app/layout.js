5baec14eae9de3c79d870ccef131bc0cd1884ed5
TeemuEsko <teemu@teemuesko.fi>
2026-05-28 13:15:23 +0300

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"asuntosijoituslaskuri.fi"};
+export default function RootLayout({children}){return <html lang="fi"><body>{children}</body></html>}
